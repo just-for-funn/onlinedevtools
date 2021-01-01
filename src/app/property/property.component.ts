9aa30ade709330735c479c2fe6544c8e29814f7a
davut <davutozcan87@gmail.com>
2021-01-01 20:34:58 +0300

    debouncing added

--- src/app/property/property.component.ts
+++ src/app/property/property.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MonacoEditorModule } from 'ngx-monaco-editor';
-import { interval, Observable } from 'rxjs';
+import { interval, Observable, Subject } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'app-property',
@@ -14,12 +15,30 @@ export class PropertyComponent implements OnInit {
     formatOnType: true,
     formatOnPaste: true
   };
-  code:string =  '';
+  
+  private _code:string = '';
+  codeSubject: Subject<string> = new Subject<string>(); 
+
   constructor() { }
 
   ngOnInit(): void {
-      interval(3000)
-          .subscribe(o=>console.log(o , this.code));
+      this.codeSubject
+        .pipe(debounceTime(500))
+        .subscribe(o=> console.log("from subject" ,o ));
   }
+
+
+   get code(){
+     return this._code;
+   }
+
+   set code(arg:string){
+     //console.log("setting" , arg);
+     this._code = arg;
+     this.codeSubject.next(arg);
+   }
+
+
+  
 
 }
