7e97e03b356d0016f0f1b4267d3b5f8324481ad7
davut <davutozcan87@gmail.com>
2021-01-03 20:45:13 +0300

    yaml env converter added

--- src/app/envconverter/yaml-to-env.spec.ts
+++ src/app/envconverter/yaml-to-env.spec.ts
@@ -4,4 +4,63 @@ describe('YamlToEnv', () => {
   it('should create an instance', () => {
     expect(new YamlToEnv()).toBeTruthy();
   });
+
+  it('should convert to env when single prop', ()=>{
+
+    let singleProp = "name: test";
+    expect(YamlToEnv.convertToEnvList(singleProp))
+       .toContain("NAME")
+      
+       expect(YamlToEnv.convertToEnvList(singleProp).length)
+       .toEqual(1);   
+  });
+
+  it('should convert nested value' , ()=>{
+    let arg = `
+      person:
+        job:
+          title: 'developer'
+    `;
+     let values = YamlToEnv.convertToEnvList(arg);
+     expect(values)
+        .toContain("PERSON_JOB_TITLE");
+  });
+
+  it('should convert nested with multiple keys' , ()=>{
+    let arg = `
+      person:
+        job:
+          title: 'developer'
+          age: '33'
+        nickname: test
+    `;
+     let values = YamlToEnv.convertToEnvList(arg);
+     expect(values).toContain("PERSON_JOB_TITLE");
+     expect(values).toContain("PERSON_JOB_AGE");
+     expect(values).toContain("PERSON_NICKNAME");
+  });
+  it('should convert arrays' , ()=>{
+    let arg = `
+      person:
+        hobbies:
+          - 'coding'
+          - 'reading'
+          - 'solving bugs'
+    `;
+     let values = YamlToEnv.convertToEnvList(arg);
+     expect(values)
+        .toContain("PERSON_HOBBIES");
+  });
+
+  it('should convert legacy syntax' , ()=>{
+    let arg = `
+      person.job.title: developer
+      person.job.age: 33
+    `;
+     let values = YamlToEnv.convertToEnvList(arg);
+     expect(values).toContain("PERSON_JOB_TITLE");
+     expect(values).toContain("PERSON_JOB_AGE");
+  });
+
+
 });
--- src/app/envconverter/yaml-to-env.ts
+++ src/app/envconverter/yaml-to-env.ts
@@ -1,2 +1,27 @@
+import {parse} from 'yaml'
 export class YamlToEnv {
+    static convertToEnvList(value: string): string[]{
+        let parsed = parse(value);
+        return Object.keys(parsed)
+            .map(o=> this.getPath(o , parsed))
+            .reduce((a,b)=>a.concat(b),[]);
+    }
+
+    static getPath(o: string, obj: any): string[] {
+        if(typeof obj[o] === 'object' && !Array.isArray(obj[o]))
+        {
+            return Object.keys(obj[o])
+                .map(k=> this.getPath(k , obj[o]))
+                .reduce((a,b)=>a.concat(b),[])
+                .map(i=> o+"_"+i)
+                .map(i=> i.toUpperCase());
+
+        }else{
+            return [YamlToEnv.normalize(o)];
+        }
+    }
+
+    private static normalize(name: string): string {
+        return name.toUpperCase().split('.').join('_');
+    }
 }
