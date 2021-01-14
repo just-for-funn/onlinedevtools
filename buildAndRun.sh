ee461b28c99acdcc1769c735b806cbfb1c95ca91
davut <davutozcan87@gmail.com>
2021-01-14 23:52:06 +0300

    online dev tools kube integration improved

--- buildAndRun.sh
+++ buildAndRun.sh
@@ -2,4 +2,8 @@ docker rmi -f $(docker images | grep devtools | awk '{split($0,a); print a[3];}'
 rm -rf dist
 npm run-script build
 docker build . --no-cache -t devtools
+
+docker tag devtools registry.digitalocean.com/dwt-development/devtools
+docker push registry.digitalocean.com/dwt-development/devtools
+
 docker run -it -p 88:80 devtools
