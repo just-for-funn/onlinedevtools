3d2162a63b99c568cf573135cf38d5f5d872464e
davut.ozcan <davut@hostfully.com>
2023-01-01 23:50:54 +0300

    small improvements

--- buildAndRun.sh
+++ buildAndRun.sh
@@ -1,9 +1,9 @@
 docker rmi -f $(docker images | grep environmentconverter | awk '{split($0,a); print a[3];}';)
 rm -rf dist
 npm run-script build
-docker build . --no-cache -t environmentconverter
+docker build . --no-cache -t environmentconverter --platform linux/arm64
 
-docker tag environmentconverter davutozcan/environmentconverter:1
-docker push davutozcan/environmentconverter:1
+docker tag environmentconverter davutozcan/environmentconverter:2
+docker push davutozcan/environmentconverter:2
 
 #docker run -it -p 88:80 environmentconverter
